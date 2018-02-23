618fe2046dc921719e187a608389936efab0e9b9
Marco Musy <marco.musy@gmail.com>
2018-02-23 13:31:25 +0100

    v6.6

---
+++ examples/readTiffStack.py
@@ -0,0 +1,33 @@
+# Example to read volumetric data in the form of a tiff stack:
+#
+# A tiff stack is a set of image slices in z. The scalar value 
+# (intensity of white) is used to create an isosurface by fixing a threshold.
+# In this example the level of white is in the range 0=black -> 150=white
+# If threshold=None this is set to 1/3 of the scalar range.
+#
+# Setting connectivity to True discards the small isolated pieces of surface
+# and only keeps the largest connected surface.
+#
+# Smoothing applies a gaussian smoothing with a standard deviation 
+# which is expressed in units of pixels.
+#
+# Backface color is set to violet (bc='v') to spot where the vtk  
+# reconstruction is (by mistake!) inverting the normals to the surface.
+#
+# If the spacing of the tiff stack is uneven in xyz, this can be corrected
+# by setting scaling factors with scaling=[xfac,yfac,zfac]
+#
+import plotter
+
+f = 'data/embryo.tif'
+
+vp = plotter.vtkPlotter(shape=(1,3))
+a0= vp.load(f, bc='v', alpha=1, threshold=80, connectivity=1, legend='connectivity=True')
+a1= vp.load(f, bc='v', alpha=1, threshold=80, connectivity=0, legend='connectivity=False')
+a2= vp.load(f, bc='v', alpha=1, smoothing=2, legend='thres=automatic\nsmoothing=2')
+
+vp.show(a0, at=0, interactive=0)
+vp.show(a1, at=1)
+vp.show(a2, at=2, interactive=1)
+
+
