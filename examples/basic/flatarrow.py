39f051c194c42dc4b6a11f5a24a4280eb43e456f
Guillaume Jacquenot <guillaume.jacquenot@gmail.com>
2019-05-21 23:23:00 +0200

    Removed trailing spaces in examples/basic/flatarrow.py

--- examples/basic/flatarrow.py
+++ examples/basic/flatarrow.py
@@ -7,7 +7,7 @@ for i in range(10):
     s, c = sin(i), cos(i)
     l1 = [[sin(x)+c,     -cos(x)+s,        x] for x in arange(0,3, 0.1)]
     l2 = [[sin(x)+c+0.1, -cos(x)+s + x/15, x] for x in arange(0,3, 0.1)]
-    
+
     FlatArrow(l1, l2, c=i, tipSize=1, tipWidth=1)
 
 show(collection(), viewup="z", axes=1, bg="w")
