129e115816e3be7fde6fa128c6974de4f0da44c9
neoglez <s1039062@stud.sbg.ac.at>
2020-03-18 15:17:16 +0100

    Code standatds fix

--- tests/common/test_shapes.py
+++ tests/common/test_shapes.py
@@ -8,7 +8,5 @@ print('vtkVersion', vtk.vtkVersion().GetVTKVersion())
 print('---------------------------------')
 
 #####################################
-arc = Arc(center=None, point1=(1, 1, 1), point2=None,
-           normal=(0, 0, 1), 
-           angle=np.pi)
+arc = Arc(center=None, point1=(1, 1, 1), point2=None, normal=(0, 0, 1), angle=np.pi)
 assert isinstance(arc, Arc)
