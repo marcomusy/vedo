6158dc8f72655e3ec5fa03cb266a2528342db7a2
neoglez <s1039062@stud.sbg.ac.at>
2020-03-18 14:57:11 +0100

    test arc creation

--- tests/common/test_shapes.py
+++ tests/common/test_shapes.py
@@ -1,1 +1,14 @@
 
+from vtkplotter import Arc
+import numpy as np
+import vtk
+
+print('---------------------------------')
+print('vtkVersion', vtk.vtkVersion().GetVTKVersion())
+print('---------------------------------')
+
+#####################################
+arc = Arc(center=None, point1=(1, 1, 1), point2=None,
+           normal=(0, 0, 1), 
+           angle=np.pi)
+assert isinstance(arc, Arc)
