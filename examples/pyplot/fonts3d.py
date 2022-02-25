904d1d2dcaa50f68d160e231a0c5dc5839037524
marcomusy <marco.musy@gmail.com>
2022-02-25 22:14:21 +0100

    fonts3d fix

--- examples/pyplot/fonts3d.py
+++ examples/pyplot/fonts3d.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from vedo import printc, Text2D, Text3D, show, Line, Plotter, shapes
+from vedo import printc, Text2D, Text3D, show, Plotter, shapes
 from vedo import fonts, fonts_path, settings
 import numpy as np
 import os
@@ -38,7 +38,24 @@ for key, props in sorted(settings.font_parameters.items()):
     i+=1
 plt0b = show(acts2d,
              Text2D("Additional fonts (https://vedo.embl.es/fonts)", pos='top-center', bg='k', s=1.3),
-             bg2='lb', size=(1300,900), pos=(1200,200), new=True,q=1)
+             bg2='lb', size=(1300,900), pos=(1200,200), new=True)
+
+################################################################################ printout
+for font in fonts:
+    printc(font + " - available characters:", " "*25, bold=1, invert=1)
+    fontfile = os.path.join(fonts_path, font + '.npz')
+    font_meshes = np.load(fontfile, allow_pickle=True)['font'][0]
+    for k in font_meshes.keys():
+        printc(k, end=' ')
+    print()
+
+printc('\n(use the above to copy&paste any char into your python script!)', italic=1)
+printc('Symbols ~ ^ _ are reserved modifiers:', italic=1)
+printc(' use ~ to add a short space, 1/4 of the default size,', italic=1)
+printc(' use ^ and _ to start up/sub scripting, space terminates them.\n', italic=1)
+printc('Supported LaTeX tags:', box='-', c='y')
+for r in shapes._reps:
+    print('\\'+repr(r[0]).replace("'","").replace("\\",""),' = ', r[1])
 
 
 ################################################################################## 3D
@@ -79,60 +96,7 @@ for i,fnt in enumerate(["Kanopus", "Normografo", "Theemim", "VictorMono"]):
              camera=cam,
              resetcam=not bool(i),
     )
+plt.interactive().close()
 
-################################################################################ printout
-for font in fonts:
-    printc(font + " - available characters:", " "*25, bold=1, invert=1)
-    fontfile = os.path.join(fonts_path, font + '.npz')
-    font_meshes = np.load(fontfile, allow_pickle=True)['font'][0]
-    for k in font_meshes.keys():
-        printc(k, end=' ')
-    print()
-
-printc('\n(use the above to copy&paste any char into your python script!)', italic=1)
-printc('Symbols ~ ^ _ are reserved modifiers:', italic=1)
-printc(' use ~ to add a short space, 1/4 of the default size,', italic=1)
-printc(' use ^ and _ to start up/sub scripting, space terminates them.\n', italic=1)
-printc('Supported LaTeX tags:', box='-', c='y')
-
-for r in shapes._reps:
-    print('\\'+repr(r[0]).replace("'","").replace("\\",""),' = ', r[1])
-printc('Font Summary', c='g', box='-')
-for i, f in enumerate(fonts):
-    printc('Font: ', f, c='g')
-
-################################################################################## 3D
-cam = dict(pos=(55.8, -4.27, 107),
-           focalPoint=(27.1, -29.2, -0.0532),
-           viewup=(-0.0642, 0.976, -0.210),
-           distance=113,
-           clippingRange=(87.1, 147))
-
-ln1 = Line([-1,-2],[52,-2], lw=0.1, c='grey')
-fn3d=[ln1]
-gap = 0
-for i, font in enumerate(fonts):
-    txt = font+": abcdefghijklmnopqrtuvwxyz 1234567890"
-    if font in ["Theemim", "Kanopus", "Normografo", "VictorMono",
-                "Galax", "LogoType", "Comae", "LionelOfParis"]:
-         txt += "\n         αβγδεζηθκλμνξπρστφχψω ΔΘΛΞΠΣΦΨΩ"
-         gap -= 2
-    if font in ["VictorMono", "Kanopus", "LogoType", "Comae","LionelOfParis"]:
-        txt+= " БГДЖЗИЙКЛ"
-    gap -= 4
-    t2 = Text3D(txt, font=font, italic=0).c(i).y(gap)
-    ln = Line([-1,gap-1],[52,gap-1], lw=0.5, c='grey')
-    fn3d.extend([t2,ln])
-
-show(fn3d,
-     new=True,
-     pos=(400,100), size=(900,900), azimuth=20,
-     axes=9,
-     camera=cam,
-     bg2='bb',
-     bg='k',
-    ).close()
-
-plt.close()
 plt0b.close()
 plt0a.close()
