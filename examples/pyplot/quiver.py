7c7f2adc2e7fee4c881c9bfff3a3c9e709010dd0
marcomusy <marco.musy@gmail.com>
2022-03-20 23:03:21 +0100

    quiver

--- examples/pyplot/quiver.py
+++ examples/pyplot/quiver.py
@@ -2,17 +2,10 @@
 from vedo import Grid, Arrows2D, show
 
 
-# create displacements
+# Create displacements
 pts1 = Grid(s=[1.0,1.0]).points()
 pts2 = Grid(s=[1.2,1.2]).rotateZ(4).points()
 
-arrs2d = Arrows2D(pts1, pts2, c="red5",
-    # shaftLength=shaftLength,
-    # shaftWidth=shaftWidth,
-    # headLength=headLength,
-    # headWidth=0.01,
-    s=0.1
-)
+quiv = Arrows2D(pts1, pts2, c="red5")
 
-
-show(arrs2d, __doc__, axes=1, zoom=1.2).close()
+show(quiv, __doc__, axes=1, zoom=1.2).close()
