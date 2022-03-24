db802338ed3f1a7ff230fcee50b4a7afa193978c
marcomusy <marco.musy@gmail.com>
2022-03-24 13:52:46 +0100

    browser example

--- examples/basic/slider_browser.py
+++ examples/basic/slider_browser.py
@@ -3,32 +3,24 @@ from vedo import settings, dataurl, load
 from vedo import Text2D, Plotter, Picture, Axes, Line
 
 def sliderfunc(widget, event):
-    global k
-    knew = int(widget.GetRepresentation().GetValue())
-    if k==knew:
-        return
-    objs[k].off()    #switch off old
-    objs[knew].on()  #switch on new
-    k = knew
-    days = int((k*2+249)/24)
-    t = f"{days}d {k*2+249 -days*24}h"
-    widget.GetRepresentation().SetTitleText(t)
+    i = int(widget.GetRepresentation().GetValue())
+    days = int((i*2+249)/24)
+    time = f"{days}d {i*2+249-days*24}h"
+    widget.GetRepresentation().SetTitleText(time)
+    # remove the old and add the new shape
+    # (no need to render as the slider makes a call to rendering)
+    plt.pop().add(objs[i], render=False)
+
+objs = load(dataurl+'timecourse1d.npy') # load a list of shapes
 
 settings.defaultFont = "Glasgo"
 
-objs = load(dataurl+'timecourse1d.npy') # load a list of shapes
-# switch off them all except the first
-for i in range(1, len(objs)):
-    objs[i].lineWidth(3).off()
-k = 0 # mesh index
-
-# show the biggest and continue (return a Plotter instance)
-plt = Plotter(bg='bb')
-plt += objs
-plt += Axes(objs[-1])
+plt = Plotter(bg='blackboard')
 plt += Text2D(__doc__, pos="top-center", s=1.2, c='w')
 plt += Picture(dataurl+'images/limbs_tc.jpg').scale(0.0154).y(10)
 plt += Line([(0,8), (0,10), (28.6,10), (4.5,8)], c='gray')
+plt += Axes(objs[-1])
+plt += objs[0]
 plt.addSlider2D(
     sliderfunc,
     0, len(objs)-1,
