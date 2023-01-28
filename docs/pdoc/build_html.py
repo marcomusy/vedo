af52b506ba9bf27d44067e346e5d425ff6ec03dd
marcomusy <marco.musy@gmail.com>
2023-01-28 17:15:13 +0100

    fix to docs/pdoc/build_html.py

--- docs/pdoc/build_html.py
+++ docs/pdoc/build_html.py
@@ -4,10 +4,11 @@ import os
 import datetime
 from vedo import __version__, printc
 
-printc("Generating documentation:\n", cmd, "\n..please wait"c='y')
 cmd =  "pdoc vedo -o html -t . "
 cmd+= f'--footer-text "version {__version__}, rev {datetime.date.today()}." '
+cmd+=  '--logo https://vedo.embl.es/images/logos/logo_vedo_simple_transp.png '
 cmd+=  '--favicon https://vedo.embl.es/images/logos/favicon.svg '
+printc("Generating documentation:\n", cmd, "\n..please wait", c='y')
 os.system(cmd)
 os.system("chmod 755 html/ -R")
 printc("Done.", c='y')
