ef303866a218cd4516f7c38c0d76e4b8e5fe0386
marcomusy <marco.musy@gmail.com>
2023-02-14 16:12:11 +0100

    add map_points_to_cells(list_array) 3

--- examples/volumetric/numpy2volume2.py
+++ examples/volumetric/numpy2volume2.py
@@ -3,13 +3,12 @@ import numpy as np
 from vedo import Volume, show
 
 data_matrix = np.zeros([70, 80, 90], dtype=np.uint8)
-data_matrix[0:30,   0:30,  0:30] = 1
+data_matrix[ 0:30,  0:30,  0:30] = 1
 data_matrix[30:50, 30:60, 30:70] = 2
-data_matrix[50:69, 60:79, 70:89] = 3
+data_matrix[50:70, 60:80, 70:90] = 3
 
 vol = Volume(data_matrix, c=['white','b','g','r'], mode=1)
 vol.add_scalarbar3d()
-vol.print()
 
 # optionally mask some parts of the volume (needs mapper='gpu'):
 # data_mask = np.zeros_like(data_matrix)
