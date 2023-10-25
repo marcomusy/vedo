23f7746cff13b2605f1a87e3db752a723d057981
marcomusy <marco.musy@gmail.com>
2023-10-25 21:38:06 +0200

    aggressive substitution to vtk.get() ugrid.py

--- vedo/ugrid.py
+++ vedo/ugrid.py
@@ -127,7 +127,7 @@ class UGrid(VolumeVisual, UGridAlgorithms):
             vedo.logger.error(f"cannot understand input type {inputtype}")
             return
 
-        self.mapper = vtk.vtkUnstructuredGridVolumeRayCastMapper()
+        self.mapper = vtk.get("UnstructuredGridVolumeRayCastMapper")()
         self.actor.SetMapper(self.mapper)
 
         self.mapper.SetInputData(self.dataset) ###NOT HERE?
@@ -235,13 +235,13 @@ class UGrid(VolumeVisual, UGridAlgorithms):
         uarr = self.dataset.GetCellTypesArray()
         ctarrtyp = np.where(utils.vtk2numpy(uarr) == ctype)[0]
         uarrtyp = utils.numpy2vtk(ctarrtyp, deep=False, dtype="id")
-        selection_node = vtk.vtkSelectionNode()
-        selection_node.SetFieldType(vtk.vtkSelectionNode.CELL)
-        selection_node.SetContentType(vtk.vtkSelectionNode.INDICES)
+        selection_node = vtk.get("SelectionNode")()
+        selection_node.SetFieldType(vtk.get("SelectionNode").CELL)
+        selection_node.SetContentType(vtk.get("SelectionNode").INDICES)
         selection_node.SetSelectionList(uarrtyp)
-        selection = vtk.vtkSelection()
+        selection = vtk.get("Selection")()
         selection.AddNode(selection_node)
-        es = vtk.vtkExtractSelection()
+        es = vtk.get("ExtractSelection")()
         es.SetInputData(0, self.dataset)
         es.SetInputData(1, selection)
         es.Update()
